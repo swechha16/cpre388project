3e2e7b2399c66442fb9b7238fa8d7a1347f739af
sghimire <sghimire@iastate.edu>
2020-11-05 15:47:43 -0600

    new files added

---
+++ app/src/main/java/com/example/slangdictionary/ListActivity.java
@@ -0,0 +1,4 @@
+package com.example.slangdictionary;
+
+public class ListActivity {
+}
--- app/src/main/java/com/example/slangdictionary/ToDoListDetailActivity.java
+++ app/src/main/java/com/example/slangdictionary/ToDoListDetailActivity.java
@@ -1,13 +1,14 @@
 package com.example.slangdictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ToDoListDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_to_do_list_detail);
     }
 }
